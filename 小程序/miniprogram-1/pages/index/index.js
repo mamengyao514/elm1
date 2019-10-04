d49a3275b9d492fbc15c3979ec6469f0e506b032
mamengyao514 <54308822+mamengyao514@users.noreply.github.com>
2019-10-04 20:50:48 +0800

    Add files via upload

--- 小程序/miniprogram-1/pages/index/index.js
+++ 小程序/miniprogram-1/pages/index/index.js
@@ -7,13 +7,96 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    name:'复制的内容'
   },
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
+  },
+
+  calling: function () {
+    //  微信自带一件拨打电话
+    //微信小程序自带  wx 代表微信小程序封装好的方法
+    //makePhoneCall  跳转手机拨号页 可以自带参数 phoneNumber  是自带参数
+    wx.makePhoneCall({
+
+      phoneNumber: '17744524232',//手机号
+      //成功回调
+      success: function () {
+
+        console.log("拨打电话成功！")
+
+      },
+      //失败回调
+      fail: function () {
+
+        console.log("拨打电话失败！")
+
+      }
+
+    })
+  },
+
+  copyText: function (e) {
+    //e 是刚才往方法里传的参数
+    console.log(e)
+    // wx.setClipboardData  微信小程序里自带方法 复制 
+    // data 是需要复制的内容
+    wx.setClipboardData({
+      data: e.currentTarget.dataset.text,
+      // 成功回调
+      success: function (res) {
+        //wx.getClipboardData  微信小程序自带方法 弹出框 
+        wx.getClipboardData({
+          success: function (res) {
+            console.log(res)
+            // wx.showToast 显示 微信小程序自带方法 tiltle 内容
+            wx.showToast({
+              title: '复制成功'
+            })
+          }
+        })
+      }
+    })
+  },
+
+  
+  //onShareAppMessage  触发微信自转发
+  onShareAppMessage: function (ops) {
+    // 判断是否 是页面的buttom 转发按钮
+    if (ops.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(ops)
+    }
+    // 弹出框 内容设置
+    return {
+      title: '转发dom',
+      path: `pages/index/index`,
+      success: function (res) {
+        // 转发成功
+        console.log(res)
+        console.log("转发成功:" + JSON.stringify(res));
+        var shareTickets = res.shareTickets;
+        // if (shareTickets.length == 0) {
+        //   return false;
+        // }
+        // //可以获取群组信息
+        // wx.getShareInfo({
+        //   shareTicket: shareTickets[0],
+        //   success: function (res) {
+        //     console.log(res)
+        //   }
+        // })
+      },
+      //转发失败 回调
+      fail: function (res) {
+        // 转发失败
+        console.log("转发失败:" + JSON.stringify(res));
+      }
+    }
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
